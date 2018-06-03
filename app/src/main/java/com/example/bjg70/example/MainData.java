ae697a333c2f7f7f71159620eca0e641fa68bdcd
bjg7039 <bjg7039@hanmail.net>
2018-06-04 00:36:54 +0900

    end

---
+++ app/src/main/java/com/example/bjg70/example/MainData.java
@@ -0,0 +1,8 @@
+package com.example.bjg70.example;
+
+/**
+ * Created by bjg70 on 2018-06-03.
+ */
+
+public class MainData {
+}
--- app/src/main/java/com/example/bjg70/example/Main_data.java
+++
@@ -1,8 +0,0 @@
-package com.example.bjg70.example;
-
-/**
- * Created by bjg70 on 2018-06-02.
- */
-
-public class Main_data {
-}
